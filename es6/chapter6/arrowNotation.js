21aa79f23666f322f67a4ea2106c0d0b0c42b3ba
Nam Won Hyung <nwh0526@gmail.com>
2018-09-08 23:29:52 +0900

    update arrowNotation.js - arrow function을 사용하여 this 문제 해결 example

--- es6/chapter6/arrowNotation.js
+++ es6/chapter6/arrowNotation.js
@@ -12,3 +12,25 @@ console.log(f2('won hyung'));
 
 const arrowF2 = (name) => `Hello, ${name}`;
 console.log(arrowF2('won hyung'));
+
+// 화살표 함수는 일반적인 함수와 다른점이 있다.
+// this가 다른 변수와 마찬가지로 정적으로(lexically) 묶인다.
+// 이전 this 예제를 arrow function을 사용하여 해결할 수 있다.
+const o = {
+    name : 'won hyung',
+    greetBackwards : function() {
+        console.log(this); // this object
+        const getReverseName = () => {
+            console.log(this); // this object
+            let nameBackwards = '';
+            for(let i=this.name.length-1; i>=0; i--) {
+                nameBackwards += this.name[i];
+            }
+            return nameBackwards;
+        }
+        return `${getReverseName()} si eman ym, olleH`;
+    }
+};
+console.log(o.greetBackwards());
+// 화살표 함수는 객체 생성자로 사용할 수 없다,
+// arguments 변수도 사용할 수 없다. 다만 확장 연산자로 대신할 수 있음.
